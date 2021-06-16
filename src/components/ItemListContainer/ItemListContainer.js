b6d6a205f225cd00de09506dab20926fd26e16c9
Jhoasil Garcia <jhoasil.info@gmail.com>
2021-06-16 19:45:05 -0300

    .

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,29 +1,26 @@
-
-import React, { useState, useEffect } from 'react';
-import ItemCount from '../ItemCount/ItemCount';
-import ItemList from '../ItemList/ItemList';
+import React, { useState, useEffect } from "react";
+import ItemCount from "../ItemCount/ItemCount";
+import ItemList from "../ItemList/ItemList";
 
 import { makeStyles } from "@material-ui/core/styles";
 
-const ItemListContainer = () => {  
+const ItemListContainer = () => {
     let available = 5;
-    const items = [];
     const useStyles = makeStyles({});
     const handleAdd = (counter) => {
-      return (evt) => {
-          evt.stopPropagation();
-      }
-    }
+        return (evt) => {
+            evt.stopPropagation();
+        };
+    };
 
     return (
-      <div>
-        <ItemCount onAdd={handleAdd} {...{ available }} initial={1} />
-        <div >
-          <ItemList/>
+        <div>
+            <ItemCount onAdd={handleAdd} {...{ available }} initial={1} />
+            <div>
+                <ItemList />
+            </div>
         </div>
-      </div>
-      );
+    );
 };
 
 export default ItemListContainer;
-
