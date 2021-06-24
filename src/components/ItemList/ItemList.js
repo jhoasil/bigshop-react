9ecf28f630746e07b346ba17ee8d1357c51a9df0
Jhoasil Garcia <jhoasil.info@gmail.com>
2021-06-24 14:06:19 -0300

    cambios

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,19 +1,15 @@
 import React, { useState, useEffect } from "react";
 import Item from "../Item/Item";
 
-import products from "../../data/products-data";
-
-const ItemList = ({ categoryId }) => {
+const ItemList = ({ products }) => {
     console.log(products);
-    console.log(categoryId);
 
     useEffect(() => {}, []);
 
     return (
         <div>
             {products.map((product) => {
-                if (categoryId == "all" || product.category == categoryId)
-                    return <Item product={product} />;
+                return <Item product={product} />;
             })}
         </div>
     );
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,20 +1,32 @@
 import React, { useState, useEffect } from "react";
 import ItemCount from "../ItemCount/ItemCount";
 import ItemList from "../ItemList/ItemList";
-
+import products from "../../data/products-data";
 import { makeStyles } from "@material-ui/core/styles";
 import { useParams } from "react-router-dom";
 
 const ItemListContainer = () => {
-    
     const { categoryId = "all" } = useParams();
     const useStyles = makeStyles({});
     console.log(categoryId);
+    let data = [];
+
+    if (categoryId == "all") {
+        data = products;
+    } else if (true) {
+        products.map((product) => {
+            if (product.category == categoryId) {
+                data.push(product);
+            }
+        });
+    }
+
+    console.log(data);
 
     return (
         <div>
             <div>
-                <ItemList categoryId={categoryId} />
+                <ItemList categoryId={categoryId} products={data} />
             </div>
         </div>
     );
